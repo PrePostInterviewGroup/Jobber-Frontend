ed5e0a6206ea28c1cfd2db3092115c12a1bfed86
Bob Holben <bholben@gmail.com>
2015-03-08 04:56:18 -0400

    Work on responsiveness of jobs page

--- app/scripts/jobs/jobs.js
+++ app/scripts/jobs/jobs.js
@@ -30,6 +30,11 @@
         $('#addJob').openModal();
       };
 
+      $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+      });
+
       $scope.submitJob = function (job) {
 
         console.log(job);
